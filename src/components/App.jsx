337b2a91cc9bd1b59a640167650a1838f2eeac72
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-10-29 12:05:36 +0000

    Loader and Button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
+
 import { getImages } from 'services/api';
+
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import { Loader } from './Loader/Loader';
+import Button from './Button/Button';
 
 export default class App extends Component {
-  // componentDidMount() {
-  //   getImages('cat', 1);
-  // }
-
   state = {
     query: '',
     page: 1,
     images: [],
+    totalImages: 0,
     showModalWindow: false,
+    loading: false,
+    error: null,
   };
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({ loading: true });
+    }
+  }
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+
+    getImages(query, page)
+      .then(response => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...response.images],
+          totalImages: response.totalImages,
+        }));
+      })
+      .catch(error => {
+        console.error('Something went wrong', error);
+        this.setState({ error: error.message });
+      });
+  };
+
   onHandleSubmit = value => {
     this.setState({
       query: value,
       page: 1,
       images: [],
+      totalImages: 0,
     });
   };
 
@@ -33,11 +65,15 @@ export default class App extends Component {
   };
 
   render() {
-    const { images } = this.state;
+    const { images, loading, totalImages } = this.state;
     return (
       <div>
         <Searchbar onSubmit={this.onHandleSubmit} />
         <ImageGallery images={images} openModalWindow={this.openModalWindow} />
+        {loading && <Loader />}
+        {totalImages !== images.length && !loading && (
+          <Button onClick={this.handleLoadMore} />
+        )}
       </div>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import css from './Searchbar.module.css';
 
 export default class Searchbar extends Component {
